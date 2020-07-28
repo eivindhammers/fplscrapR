8e75e7c41dd536660133f8ec3c5cc0d457adf4c7
Eivind Hammersmark <eivindhammers@gmail.com>
2020-07-28 22:45:09 +0200

    Add entry ID to output of get_entry_season

--- R/entry_functions.R
+++ R/entry_functions.R
@@ -61,7 +61,7 @@ get_entry_season <- function(entryid = NULL){
       for (i in 1:length(entryid)){
         entry <- jsonlite::fromJSON(paste("https://fantasy.premierleague.com/api/entry/",entryid[i],"/",sep=""))
         entryseason <- jsonlite::fromJSON(paste("https://fantasy.premierleague.com/api/entry/",entryid[i],"/history","/",sep=""))
-        entryseason2 <- rbind(entryseason2,data.frame(entryseason$current,name=paste(entry$player_first_name,entry$player_last_name)))
+        entryseason2 <- rbind(entryseason2,data.frame(entryseason$current,entry=entryid[i],name=paste(entry$player_first_name,entry$player_last_name)))
       }
       return(entryseason2)
     }
@@ -106,7 +106,7 @@ get_entry_picks <- function(entryid = NULL, gw = NULL){
 get_entry_player_picks <- function(entryid = NULL, gw = NULL){
   if(is.null(entryid)) stop("You'll need to input an entry ID, mate.")
   if(is.null(gw)) stop("You'll need to input a gameweek, mate.")
-  
+
   picks3 <- data.frame()
   for (j in 1:length(entryid)) {
     picks2 <- data.frame()
@@ -115,20 +115,20 @@ get_entry_player_picks <- function(entryid = NULL, gw = NULL){
     for (i in 1:length(gw_thisentry)) {
       picks_list <- jsonlite::fromJSON(paste("https://fantasy.premierleague.com/api/entry/",entryid[j],"/event/",gw_thisentry[i],"/picks","/",sep=""))
       picks <- picks_list$picks
-    
+
       picks$event <- gw_thisentry[i]
       picks2 <- rbind(picks2, picks)
     }
     picks2$entry <- entryid[j]
     picks3 <- rbind(picks3, picks2)
   }
-  
+
   player_names <- get_player_name(unique(picks3$element))
   picks3 <- merge(player_names, picks3, by.x = "id", by.y = "element")
   names(picks3) <- c("id", "playername", "position", "multiplier", "is_captain", "is_vice_captain", "event", "entry")
-  
+
   picks3 <- picks3[order(picks3$entry, picks3$event), ]
-  
+
   return(picks3)
 }
 
@@ -148,7 +148,7 @@ get_entry_player_picks <- function(entryid = NULL, gw = NULL){
 get_entry_captain <- function(entryid = NULL, gw = NULL){
   if(is.null(entryid)) stop("You'll need to input an entry ID, mate.")
   if(is.null(gw)) stop("You'll need to input a gameweek, mate.")
-  
+
   captain3 <- data.frame()
   for (j in 1:length(entryid)) {
     captain2 <- data.frame()
@@ -166,12 +166,12 @@ get_entry_captain <- function(entryid = NULL, gw = NULL){
     captain2$entry <- entryid[j]
     captain3 <- rbind(captain3, captain2)
   }
-  
+
   player_names <- get_player_name(unique(captain3$captain))
   captain3 <- merge(captain3, player_names, by = "id")
   captain3 <- captain3[c(1, 4, 3, 2)]
   names(captain3) <- c("id", "playername", "entry", "event")
   captain3 <- captain3[order(captain3$entry,captain3$event), ]
-  
+
   return(captain3)
 }
